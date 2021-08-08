87c0c31a9bc73a308fcefd470d3e1eeb6a241da8
celestialturnip <celestialturnip@protonmail.com>
2021-08-08 14:57:48 +0900

    Add changing face color on start menu

--- menus/start_menu.gd
+++ menus/start_menu.gd
@@ -1,5 +1,8 @@
 extends Control
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("ui_accept"):
-		get_tree().change_scene("res://levels/sandbox.tscn")
+		assert(!get_tree().change_scene("res://levels/sandbox.tscn"))
+
+func _on_Timer_timeout():
+	$CenterContainer/HBoxContainer/TextureRect.modulate = Utils.get_random_color()
--- utils/utils.gd
+++ utils/utils.gd
@@ -1,4 +1,11 @@
 extends Node
 
+var palette = ["#000000","#0022c7","#002bfb","#d62816","#ff331c","#d433c7","#ff40fc","#00c525","#00f92f","#00c7c9","#00fbfe","#ccc82a","#fffc36","#cacaca","#ffffff"]
+var virtual_height = ProjectSettings.get("display/window/size/height")
+var virtual_width = ProjectSettings.get("display/window/size/width")
+
 func _ready():
 	randomize()
+
+func get_random_color():
+	return palette[randi() % palette.size()]
