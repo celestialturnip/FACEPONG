dfa76c62bfde9a85cc173b25a8bd0ebf2764f477
celestialturnip <celestialturnip@protonmail.com>
2021-08-08 13:31:11 +0900

    Add ball to main instances when ready

--- ball/ball.gd
+++ ball/ball.gd
@@ -1,7 +1,16 @@
 extends KinematicBody2D
+class_name Ball
 
-var speed = 100
-var velocity = Vector2(rand_range(-1, 1), rand_range(-1, 1)).normalized() * speed
+var MainInstances = ResourceLoader.MainInstances
+
+var speed = 150
+var velocity = Vector2(rand_range(-0.3, .3), rand_range(-1, 1)).normalized() * speed
+
+func _ready():
+	MainInstances.Ball = self
+
+func _exit_tree():
+	MainInstances.Ball = null
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
