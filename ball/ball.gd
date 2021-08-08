bbc11c87e57581828583b7db4f6fff9c2876b735
celestialturnip <celestialturnip@protonmail.com>
2021-08-08 14:54:07 +0900

    Add ball reset ability

--- ball/ball.gd
+++ ball/ball.gd
@@ -3,14 +3,23 @@ class_name Ball
 
 var MainInstances = ResourceLoader.MainInstances
 
-var speed = 150
-var velocity = Vector2(rand_range(-0.3, .3), rand_range(-1, 1)).normalized() * speed
+var speed = 110
+var velocity = Vector2.ZERO
 
 func _ready():
 	MainInstances.Ball = self
+	set_velocity()
 
 func _exit_tree():
 	MainInstances.Ball = null
+
+func _process(_delta):
+	if Input.is_action_just_pressed("reset"):
+		position = Vector2(Utils.virtual_width / 2, Utils.virtual_height / 2)
+		set_velocity()
+
+func set_velocity():
+	velocity = Vector2(rand_range(-0.3, .3), rand_range(-1, 1)).normalized() * speed
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
