4be38855eb83c8a90426d5166b020e289c4547f2
celestialturnip <celestialturnip@protonmail.com>
2021-08-09 10:07:20 +0900

    Add first level

--- menus/start_menu.gd
+++ menus/start_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_accept"):
-		assert(!get_tree().change_scene("res://levels/sandbox.tscn"))
+		assert(!get_tree().change_scene("res://levels/level01.tscn"))
 
 func _on_Timer_timeout():
 	$CenterContainer/HBoxContainer/TextureRect.modulate = Utils.get_random_color()
