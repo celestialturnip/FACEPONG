23b8f156eeb32f7e4c467d9f0778c9904ebb3ad2
celestialturnip <celestialturnip@protonmail.com>
2021-08-24 14:13:01 +0900

    Fix level01 always loading when user restarts level

--- menus/pause_menu.gd
+++ menus/pause_menu.gd
@@ -3,11 +3,10 @@ extends CanvasLayer
 var label_idx = 0
 var paused = false setget set_paused
 
-onready var labels = [
-	$ColorRect/HBoxContainer/VBoxContainer/ResumeLabel,
-	$ColorRect/HBoxContainer/VBoxContainer/RestartLabel,
-	$ColorRect/HBoxContainer/VBoxContainer/ExitLabel
-]
+onready var resume_label = $ColorRect/HBoxContainer/VBoxContainer/ResumeLabel
+onready var restart_label = $ColorRect/HBoxContainer/VBoxContainer/RestartLabel
+onready var exit_label = $ColorRect/HBoxContainer/VBoxContainer/ExitLabel
+onready var labels = [resume_label, restart_label, exit_label]
 
 func _ready() -> void:
 	Utils.toggle(labels[label_idx], true)
@@ -18,13 +17,14 @@ func set_paused(value):
 	$ColorRect.visible = paused
 
 func _process(_delta: float) -> void:
+	var current_label = labels[label_idx]
 	if Input.is_action_just_pressed("ui_cancel") and get_tree().get_nodes_in_group("Player").size() > 0:
+		reset_labels()
 		self.paused = !paused
 
 	if not $ColorRect.visible:
 		return
 
-	var current_label = labels[label_idx]
 	if Input.is_action_just_pressed("ui_down"):
 		Utils.toggle(labels[label_idx], false)
 		label_idx = (label_idx + 1) % labels.size()
@@ -38,13 +38,18 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		self.paused = !paused
 		SoundFX.play("menu_accept.wav")
-		label_idx = 0  # Reset it back to zero so next time player opens pause, the first label is selected.
-		match current_label.text:
-			"EXIT":
+		reset_labels()
+		match current_label:
+			exit_label:
 				# warning-ignore:return_value_discarded
 				get_tree().change_scene("res://menus/start_menu.tscn")
-			"RESTART":
+			restart_label:
 				# warning-ignore:return_value_discarded
-				get_tree().change_scene("res://levels/level01.tscn")
-			"RESUME":
+				get_tree().change_scene(Utils.previous_level_scene_path)
+			resume_label:
 				pass
+
+func reset_labels():
+	Utils.toggle(labels[label_idx], false)
+	label_idx = 0
+	Utils.toggle(labels[label_idx], true)
