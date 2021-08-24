de631a9efcde56a687fb4dd51490864313b97722
celestialturnip <celestialturnip@protonmail.com>
2021-08-24 12:43:54 +0900

    Refactor options menu

--- menus/options_menu.gd
+++ menus/options_menu.gd
@@ -1,20 +1,42 @@
 extends Control
 
+var label_idx = 0
 onready var crt_label = $CenterContainer/VBoxContainer/CRTLabel
+onready var back_label = $CenterContainer/VBoxContainer/BackLabel
+onready var labels = [crt_label, back_label]
 
 func _ready():
-	crt_label.add_color_override("font_color", Utils.colors["gold"])
-	crt_label.text = "CRT: " + ("ON" if CRT.is_active() else "OFF")
+	toggle(labels[label_idx], true)
 
 func _process(_delta):
-	if Input.is_action_just_pressed("ui_cancel"):
-		SoundFX.play("menu_navigation.wav")
-		# warning-ignore:return_value_discarded
-		get_tree().change_scene("res://menus/start_menu.tscn")
-	if Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
-		SoundFX.play("menu_navigation.wav")
-		toggle(crt_label)
+	var current_label = labels[label_idx]
+	
+	match current_label:
+		crt_label:
+			if Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
+				SoundFX.play("menu_navigation.wav")
+				CRT.toggle()
+				crt_label.text = "CRT: ON" if CRT.is_active() else "CRT: OFF"
+		back_label:
+			if Input.is_action_just_pressed("ui_accept"):
+				SoundFX.play("menu_accept.wav")
+				# warning-ignore:return_value_discarded
+				get_tree().change_scene("res://menus/start_menu.tscn")
 
-func toggle(_label):
-	CRT.toggle()
-	crt_label.text = "CRT: " + ("ON" if CRT.is_active() else "OFF")
+	if Input.is_action_just_pressed("ui_down"):
+		toggle(current_label, false)
+		label_idx = (label_idx + 1) % len(labels)
+		toggle(labels[label_idx], true)
+	elif Input.is_action_just_pressed("ui_up"):
+		toggle(current_label, false)
+		label_idx = (label_idx - 1) % len(labels)
+		toggle(labels[label_idx], true)
+
+func toggle(label, on: bool):
+	if not on:
+		label.add_color_override("font_color", Utils.colors["white"])
+		SoundFX.play("menu_navigation.wav")
+		
+	else:
+		label.add_color_override("font_color", Utils.colors["orange"])
+
